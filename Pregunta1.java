0eea78452df61da342ead8c89312b5c49c73304a
Fernando Mejías Pando <113796719+Fmejias03@users.noreply.github.com>
2023-02-02 19:20:57 +0000

    Tarea1

--- Pregunta1.java
+++ Pregunta1.java
@@ -1,6 +1,23 @@
-//¿Qué algoritmos usas en tu vida cotidiana? 
-//
-// //codigo de java para el algoritmo de ordenamiento 
+// ¿Que algoritmos usas en tu vida cotidiana?
+
+// Algoritmos de ordenamiento
+// Algoritmos de busqueda
+// Algortimos de grafos
+// Algoritmos de programacion dinamica
+// Algoritmos de programacion orientada a objetos
+// Algoritmos de programacion estructurada
+// Algoritmos de programacion funcional
+
+//¿Piensas que podrías escribir un programa para hacerlos más eficientes?
+
+// Si, se puede escribir un programa para hacerlos mas eficientes
+// Si, pero no tengo tiempo
+// Si, pero no tengo conocimientos
+// Si, pero no tengo recursos
+// Si, pero no tengo motivacion
+
+
+
 
 
 
--- Pregunta2.java
+++ Pregunta2.java
@@ -1,31 +1,20 @@
 // ¿Qué algoritmos crees que usan por tus juegos y aplicaciones favoritas?
 
-//codigo de java para el algoritmo de ordenamiento de burbuja de forma recursiva con programacion orientada a objetos
+// Algoritmos de ordenamiento
+// Algoritmos de busqueda
+// Algortimos de grafos
+// Algoritmos de programacion dinamica
 
-public class OrdenamientoBurbujaRecursivo {
+//codigo de java
+
+class Pregunta2 {
     public static void main(String[] args) {
-        int[] arreglo = {5, 3, 2, 4, 7, 1, 0, 6};
-        OrdenamientoBurbujaRecursivo burbuja = new OrdenamientoBurbujaRecursivo();
-        burbuja.ordenar(arreglo, arreglo.length);
-        burbuja.imprimir(arreglo);
+        System.out.println("¿Que algoritmos crees que usan por tus juegos y aplicaciones favoritas?");
+        System.out.println("Algoritmos de ordenamiento");
+        System.out.println("Algoritmos de busqueda");
+        System.out.println("Algortimos de grafos");
+        System.out.println("Algoritmos de programacion dinamica");
     }
-    public void ordenar(int[] arreglo, int n) {
-        if (n == 1) {
-            return;
-        }
-        for (int i = 0; i < n - 1; i++) {
-            if (arreglo[i] > arreglo[i + 1]) {
-                int temp = arreglo[i];
-                arreglo[i] = arreglo[i + 1];
-                arreglo[i + 1] = temp;
-            }
-        }
-        ordenar(arreglo, n - 1);
-    }
-    public void imprimir(int[] arreglo) {
-        for (int i = 0; i < arreglo.length; i++) {
-            System.out.print(arreglo[i] + " ");
-        }
-    }
-}
+
+
 
